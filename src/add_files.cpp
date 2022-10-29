ec618460d43ae00037d917981d566394d3a57cf3
brian8181 <brian8181@gmail.com>
2022-10-29 06:48:51 -0500

    continue with add_files function

--- src/add_files.cpp
+++ src/add_files.cpp
@@ -1,44 +1,40 @@
 #include <iostream>
 #include <string>
 #include <fstream>
-// #include <regex>
 #include <getopt.h>
 #include "main.hpp"
 #include "music_sync.hpp"
-
-using namespace std;
-
-// int parse_options(int argc, char* argv[])
-// {
-// 	cout << "Hello Template!" << endl;
-// 	return 0;
-// }
-
 #include <fstream>
 #include <iostream>
 #include <filesystem>
+
+using namespace std;
+
  
 int parse_options(int argc, char* argv[])
 {
-    const std::filesystem::path sandbox{"/home/brian/Music"};
+    if(argc < 4)
+        return -1;
+        
+    string music_lib_path = "/home/brian/Music";
+    const std::filesystem::path sandbox{music_lib_path};
     for (auto const& dir_entry : std::filesystem::recursive_directory_iterator{sandbox}) 
     {
         std::cout << dir_entry << '\n';
     }
     
+    string sync_file_path = "tmp.txt";
+    ofstream file;
+    file.open(sync_file_path);
+    cout << "writing to file \"../" << sync_file_path << "\"";
 
-    if(argc <  2)
-        return -1;
-
-    string sync_list;
-    
     for(int i = 1; i < argc; i++)
     {
-        sync_list + argv[i] + "/n";
+        cout << argv[i] << endl;
+        file << argv[i]  << endl;
     }
     
-    ofstream of;
-    
+    file.close();
 
 	return 0;
 }
--- src/music_sync.cpp
+++ src/music_sync.cpp
@@ -1,21 +1,10 @@
 #include <iostream>
-// #include <string>
-// #include <regex>
 #include <getopt.h>
 #include "main.hpp"
-#include "music_sync.hpp"
-
-// using namespace std;
-
-// int parse_options(int argc, char* argv[])
-// {
-// 	cout << "Hello Template!" << endl;
-// 	return 0;
-// }
-
 #include <fstream>
 #include <iostream>
 #include <filesystem>
+#include "music_sync.hpp"
  
 int parse_options(int argc, char* argv[])
 {
